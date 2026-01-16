4f3f04a216de7eea8f120449b3f93b1b1b05985b
atikroman2019 <atikroman2016@gmail.comgmail.com>
2026-01-16 22:57:20 +0600

    MongoDb Connection Setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -9,12 +10,73 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// test route
+// Build MongoDB URI from env
+console.log("DB_CLUSTER:", process.env.DB_CLUSTER);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db(process.env.DB_NAME);
+    const visaCollection = database.collection("visas");
+    const applicationCollection = database.collection("applications");
+
+    // Test DB route
+    app.get("/test-db", async (req, res) => {
+      const count = await visaCollection.estimatedDocumentCount();
+      res.send({ success: true, count });
+    });
+
+    console.log("MongoDB connected securely ✅");
+  } catch (error) {
+    console.error("MongoDB connection failed ❌", error);
+  }
+}
+
+run();
+
+// Root route
 app.get("/", (req, res) => {
   res.send("Visa Navigator Server Running 🚀");
 });
 
-// start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://atikroman2016_db_user:<db_password>@cluster0.w6jd5gu.mongodb.net/?appName=Cluster0";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
